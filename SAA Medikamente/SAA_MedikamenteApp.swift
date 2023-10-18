2bec4de1f816eee385774c268b5ae6c86177cb81
Laurin Bräunig <info@applications.laurinbraeunig.com>
2023-10-18 11:37:46 +0200

    Initial Commit

---
+++ SAA Medikamente/SAA_MedikamenteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAA_MedikamenteApp.swift
+//  SAA Medikamente
+//
+//  Created by Laurin Bräunig on 18.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SAA_MedikamenteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
